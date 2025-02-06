c52655e05b2a783827e0ead6fbecbba050d2901a
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-02-06 00:27:36 -0500

    Refactor MyApp to use router configuration for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app_rec_arch/repositories/note_repository.dart';
+import 'package:notes_app_rec_arch/routes/routes.dart';
 import 'package:notes_app_rec_arch/screens/note_list_screen.dart';
 import 'package:notes_app_rec_arch/viewmodels/note_viewmodel.dart';
 import 'package:provider/provider.dart';
@@ -24,28 +25,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const NoteListScreen(),
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
